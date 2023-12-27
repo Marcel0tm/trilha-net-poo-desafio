fc46244dd4bd7f8f148de558c15e6f360ae8f18e
Talisson Marcelo <marcelosilvatmls@gmail.com>
2023-12-27 16:14:54 -0300

    Realizado todos os TODO's e finalizando o desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("\n");
+
+Nokia nokia = new Nokia("123","Nokia Tijolão", "733849", 1024);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Memes do Facebook");
+
+Console.WriteLine("\n");
+
+Iphone iphone = new Iphone("763920", "iPhone 20 Pro Ultra Max Plus", "10203040", 512);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Rotas Malucas do Waze");
